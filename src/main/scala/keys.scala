b28893011de044830c13ecb4349b84f9887e145c
softprops <d.tangren@gmail.com>
2013-08-20 22:23:38 -0400

    scoping task and way to override version of caliper version used

--- src/main/scala/keys.scala
+++ src/main/scala/keys.scala
@@ -1,8 +1,10 @@
 package cappi
 
-import sbt.{ InputKey, TaskKey }
+import sbt.{ InputKey, SettingKey, TaskKey }
 
 object Keys {
+  val cappi = TaskKey[String]("cappi", "cappi is a front end for the caliper microbenchmarking toolkit")
+  val caliperVersion = SettingKey[String]("caliper-version", "Version of caliper to depend on in test. Default is (0.5-rc1)")
   val benchmark = TaskKey[Unit]("benchmark", "Executes all benchmarks.")
   val benchmarkOnly = InputKey[Unit]("benchmark-only", "Executes specified benchmarks.")
 }
