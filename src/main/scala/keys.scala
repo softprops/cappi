a23f827dcd20f742b7523c4dc3de4c1c49d5ca94
softprops <d.tangren@gmail.com>
2013-10-05 13:39:14 -0400

    caliper options

--- src/main/scala/keys.scala
+++ src/main/scala/keys.scala
@@ -4,6 +4,7 @@ import sbt.{ InputKey, SettingKey, TaskKey }
 
 object Keys {
   val cappi = TaskKey[String]("cappi", "cappi is a front end for the caliper microbenchmarking toolkit")
+  val caliperOptions = SettingKey[Seq[String]]("caliper-options", "List of options for a single caliper run")
   val caliperVersion = SettingKey[Option[String]]("caliper-version", "Version of caliper to depend on in test. Default is Some(0.5-rc1)")
   val benchmark = TaskKey[Unit]("benchmark", "Executes all benchmarks.")
   val benchmarkOnly = InputKey[Unit]("benchmark-only", "Executes specified benchmarks.")
